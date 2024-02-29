2673c498ba189d258c0bafb1d804621c3d70ca43
Andre Bun <andreebun97@gmail.com>
2024-02-29 14:54:02 +0700

    Added cashback and card master features

--- application/views/templates/js_list.php
+++ application/views/templates/js_list.php
@@ -6,6 +6,11 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
 <!-- jQuery Custom Scroller CDN -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.concat.min.js"></script>
+<script src="<?php echo base_url() . 'assets/js/inputmask/inputmask.js';?>"></script>
+<script src="<?php echo base_url() . 'assets/js/inputmask/jquery.inputmask.js';?>"></script>
+<script src="<?php echo base_url() . 'assets/js/inputmask/inputmask.date.extensions.js';?>"></script>
+<script src="<?php echo base_url() . 'assets/js/inputmask/inputmask.numeric.extensions.js';?>"></script>
+<script src="<?php echo base_url() . 'assets/js/currency.js';?>"></script>
 
 <script type="text/javascript">
     $(document).ready(function () {
