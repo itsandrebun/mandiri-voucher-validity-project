81f2f49ee80e862441c3f70c4f1e6947102d7c2e
Andre Bun <andreebun97@gmail.com>
2024-03-06 16:31:52 +0700

    Added blurred ID Card Number and Card Number field value

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -31,4 +31,44 @@ $(document).ready(function() {
     $('.upper-case-input').on('keyup', function(){
         $(this).val($(this).val().toUpperCase());
     });
+
+    $('input[name="customer_phone"]').on('keyup', function(){
+        customer_phone_number = $(this).val();
+    
+        customer_phone_number = validate_numeric(customer_phone_number);
+    
+        $(this).val(customer_phone_number);
+    });
+
+    $('.seen_value_button').click(function(){
+        var private_input_type = $(this).parent().siblings('.private-input-only').css('-webkit-text-security');
+
+        if(private_input_type == "square"){
+            $(this).parent().siblings('.private-input-only').css('-webkit-text-security','none')
+        }else{
+            $(this).parent().siblings('.private-input-only').css('-webkit-text-security','square')
+        }
+    })
+
+    $('input.private-input-only').on('keyup', function(){
+        digit_value = $(this).val();
+    
+        digit_value = validate_numeric(digit_value, "non_phone_number");
+    
+        $(this).val(digit_value);
+    });
+
+    function validate_numeric(input_value, input_name = "phone_number"){
+        input_value = input_value.replace(/\D/g,'');
+
+        if(input_name == "phone_number"){
+            if(input_value.charAt(0) == "0"){
+                input_value = input_value.substring(1);
+            }else if(input_value.substring(0,2) == "62"){
+                input_value = input_value.substring(2);
+            }
+        }       
+
+        return input_value;
+    }
 });
